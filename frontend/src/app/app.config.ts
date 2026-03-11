19bcb172e73db3436887f6f0939c1080bdcd3116
RobotDRMX <hailrobot05@gmail.com>
2026-03-11 04:01:31 -0600

    feat: register AuthInterceptor globally in Angular app config

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -2,14 +2,18 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { routes } from './app.routes';
+import { authInterceptor } from './interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideClientHydration(),
     provideAnimations(),
-    provideHttpClient(withFetch())
-  ]
+    provideHttpClient(
+      withFetch(),
+      withInterceptors([authInterceptor]),
+    ),
+  ],
 };
